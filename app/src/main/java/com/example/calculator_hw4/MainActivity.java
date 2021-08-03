4c71e48e9bcbf0529e9dc09f0585c9ab0aa552a1
shalva <smkhetsadze@gmail.com>
2021-08-03 16:43:03 +0400

    start calculator app

--- app/src/main/java/com/example/calculator_hw4/MainActivity.java
+++ app/src/main/java/com/example/calculator_hw4/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.calculator_hw4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.location.GnssAntennaInfo;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
